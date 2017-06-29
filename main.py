88a615a3945a1d0594baa8fa982ae0bc3d102eeb
Sean Stevens <sstevens88@gmail.com>
2017-06-28 21:43:20 -0700

    Added pubdate and corrected conditional classes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.add_template_global(len, name='len')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:admin@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
@@ -29,10 +30,15 @@ def bloglist():
 
     if blog_id:
         blog = Blog.query.get(blog_id)
-        return render_template('blog.html', blog_title = blog.title, blog_body = blog.body)
+        return render_template('blog.html', 
+                               blog_title=blog.title, 
+                               blog_body=blog.body,
+                               pub_date=blog.pubdate,)
     else:
         blogs = Blog.query.order_by(Blog.pubdate.desc()).all()
-        return render_template('bloglist.html', title = 'Bloglist', blogs = blogs)
+        return render_template('bloglist.html', 
+                               title='Bloglist', 
+                               blogs=blogs,)
 
 
 @app.route('/newpost', methods = ['POST', 'GET'])
@@ -56,11 +62,17 @@ def newpost():
             blog_id = new_blog.id
             return redirect('/blog?id={0}'.format(blog_id))
         else:
-            return render_template('newpost.html', title = 'New Blog Post', blog_body = blog_body,
-                                blog_title = blog_title, title_error = title_error, body_error = body_error)
+            return render_template('newpost.html', 
+                                   title='New Blog Post', 
+                                   blog_body=blog_body, 
+                                   blog_title=blog_title, 
+                                   title_error=title_error, 
+                                   body_error=body_error,)
 
     placeholder = random.choice(["What's on your mind?", "How're you doing today?", "This message is randomly generated!"])
-    return render_template('newpost.html', title = 'New Blog Post', placeholder = placeholder)
+    return render_template('newpost.html', 
+                           title='New Blog Post', 
+                           placeholder=placeholder,)
 
 
 if __name__ == '__main__':
