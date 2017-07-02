ad0515a1af555721178d5f4a462f9ec73015bba5
Sean Stevens <sstevens88@gmail.com>
2017-07-02 10:37:40 -0700

    Finished character counter

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,20 @@
-document.getElementById('textarea').onkeyup = function () {
-    document.getElementById('count').innerHTML = "Characters left: " + (500 - this.value.length);
+window.onload = charCounter;
+
+function charCounter() {
+    var text = document.getElementById('textarea');
+    var counter = document.getElementById('counter');
+
+    document.getElementById('counter').innerHTML = (500 - text.value.length);
+
+    function count() {
+        counter.innerHTML = (500 - this.value.length);
+        if (this.value.length >= 450) {
+            counter.style.color = "#f44336";
+        } else {
+            counter.style.color = "#3f3f43";
+        };
+    };
+
+    text.addEventListener('keyup', count);
+    text.addEventListener('keydown', count);
 };
